09e9089902040e02de7ece278398af6deb465792
Eric Heidelmark <hmark6@gmail.com>
2009-04-24 11:06:00 -0400

    Unit testing Users added. Schweet.

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -9,8 +9,33 @@ class UserTest < Test::Unit::TestCase
   end
   
   def test_unique_username
-  	@user_a = users(:user_01)
-  	@user_b = users(:user_02)
+  	user_a = users(:user_01)
+  	user_b = users(:user_02)
+  	
+  	assert !user_b.valid?
+  	assert_equal 'Username taken.', user_b.errors.on(:username)
+  	
+  end
+  
+  def test_password
+  	ok = %w{ password }
+  	bad = %w{ foo foofoofoofoofoofoofoofoofoofoofoofoofoofoo }
+  	
+  	# test invalid passwords
+  	bad.each do |pwd|
+  		user = User.new(:username => 'thisGuy', :password => pwd, :password_confirmation => pwd)
+  		assert !user.valid?, "trying #{pwd}"
+  	end
+  	
+  	# test valid password(s)
+  	ok.each do |pwd|
+  		user = User.new(:username => 'thisGuy', :password => pwd, :password_confirmation => pwd)
+  		assert user.valid?
+  	end
+  	
+  	# test unconfirmed password(s)
+  	user = User.new(:username => 'thisGuy', :password => 'password', :password_confirmation => '')
+  	assert !user.valid?, "trying unconfirmed"
   	
   end
   
